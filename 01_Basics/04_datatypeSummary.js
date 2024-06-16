3002a491f3d27e16b3476fb778d5807bb607479d
harshtank25 <harshtank2508@gmail.com>
2024-06-16 11:04:38 +0530

    Datatypes in JS

---
+++ 01_Basics/04_datatypeSummary.js
@@ -0,0 +1,8 @@
+// on basis of How data is to be storeed in memory its divided into two categories
+// Primitive and Non-primitive.
+
+// Primitive (Call by value) ==> 7 Types: String,Numbers,Boolean,null,undefined,Symbol,Bigint
+
+// Non-Primitive (Refrence Type) ==> Arrays,Objects,Fucntions
+
+// Js is 
